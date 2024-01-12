9381eeb86a8b8c4435f2f6825a9869f2216a8d6a
dmistry <84239029+Dm1stry@users.noreply.github.com>
2024-01-12 14:36:18 +0300

    Add files via upload

--- intel_realsense_installer.sh
+++ intel_realsense_installer.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/IntelRealSense/librealsense.git
+cd librealsense
 
 sudo apt-get install cmake libusb-1.0-0-dev pkg-config libgtk-3-dev libssl-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev at libudev-dev 
 
@@ -6,7 +7,9 @@ mkdir build
 
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=true ..
+dir=$(pwd)
+
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=true "$(dirname "$dir")"
 
 cmake --build .
 
